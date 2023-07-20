4ea48d5404f5a9c02563301ebf19e3f2a6237345
Alex <ignalex@users.noreply.github.com>
2023-07-20 06:43:41 +0300

    btn

--- btn.py
+++ btn.py
@@ -8,16 +8,16 @@ class Button:
         self.min_ago = min_ago
         self.ID = id
         self.counter = 0
+        self.pressed = False
 
         self._next_call = time.ticks_ms() #+ self.min_ago
-        # self._long_press = None
 
         pin.irq(trigger=trigger, handler=self.debounce_handler)
 
     def call_callback(self, pin):
         print('btn {} pressed'.format(self.ID))
         self.counter += 1
-        self.callback(pin, self.ID, self.counter)
+        self.callback(pin)
 
     def debounce_handler(self, pin):
         if time.ticks_ms() > self._next_call:
@@ -26,6 +26,40 @@ class Button:
         else:
             print('btn {} bouncing - not processed'.format(self.ID))
             
+class Button2:
+    def __init__(self, pin, callback, min_ago=500, id = None):
+        # Pin.IRQ_FALLING | IRQ_RISING
+        self.callback = callback
+        self.min_ago = min_ago
+        self.ID = id
+        self.counter = 0
+        self.pressed = False
+
+        self._next_call = time.ticks_ms() #+ self.min_ago
+
+        pin.irq(trigger=Pin.IRQ_FALLING, handler=self.debounce_handler)
+        pin.irq(trigger=Pin.IRQ_RISING,  handler=self.release_handler)
+
+    def call_callback(self, pin):
+        print('btn {} pressed'.format(self.ID))
+        self.counter += 1
+        self.pressed = True
+        self.callback(pin)
+
+    def debounce_handler(self, pin):
+        if time.ticks_ms() > self._next_call and not self.pressed:
+            self._next_call = time.ticks_ms() + self.min_ago
+            self.call_callback(pin)
+        else:
+            print('btn {} bouncing - not processed'.format(self.ID))            
+            
+    def release_handler(self, pin):
+        if self.pressed: 
+            self.pressed = False
+            print(self.ID + ' released')
+
+
+
 def test(x):
     print('test ' + str(x))
     
